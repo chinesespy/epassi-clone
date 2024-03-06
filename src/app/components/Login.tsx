b9f7f5e4927a76d67107fc7df8cfd2d2ee255dfe
КОШКА <57541058+chinesespy@users.noreply.github.com>
2024-03-06 22:49:22 +0200

    d

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -31,9 +31,6 @@ const Form = () => {
         <div className="container h-[15rem] flex justify-center items-center bg-neutral-50 w-[20rem] rounded-xl shadow">
 
             <form onSubmit={onSubmit}>
-                <div className='relative flex justify-center items-center mb-16 pt-4 '> 
-                    <EPassiLogo className="w-16 h-16 absolute"></EPassiLogo>
-                </div>
                 <div className="relative z-0 w-full mb-5 group">
                     <input type="text" name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-300 peer" placeholder="" required />
                     <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-300 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
